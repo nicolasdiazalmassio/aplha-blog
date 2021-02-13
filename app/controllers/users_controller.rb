757b2619355e6a78998bc67e5a0ec6981a287501
nicolas diaz almassio <nicolas.diaz.almassio@gmail.com>
2021-02-13 08:50:22 -0300

    added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
         @articles = @user.articles    
     end
 
+    def index
+        @users = User.all
+    end
+
+
     def create
         @user = User.new(user_params)
         if @user.save 
